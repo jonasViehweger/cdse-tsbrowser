8b20bf624b1cc1cc3f22346653c478e09e0cd757
jonasViehweger <53001455+jonasViehweger@users.noreply.github.com>
2026-03-27 19:15:58 +0100

    Root as base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  base: '/cdse-tsbrowser/',
+  base: '/',
   plugins: [vue(), cloudflare()],
 })
